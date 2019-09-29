3c1e7cfb46d3819ed857c8797d6da2632ef8935b
FernandoEdwinReyesReyes <fernandoreyesreyes1@gmail.com>
2019-09-29 13:57:11 -0500

    1 Cambio

--- FormResgistrar/Form1.cs
+++ FormResgistrar/Form1.cs
@@ -20,7 +20,7 @@ namespace FormResgistrar
         private void Form1_Load(object sender, EventArgs e)
         {
             Console.WriteLine("");
-
+            Console.ReadLine();
 
         }
     }
